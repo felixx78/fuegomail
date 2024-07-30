15b10d1e0e5ce76942adbc3260e3f8ca4d0ed0c8
felixx78 <petrenkov605@gmail.com>
2024-07-30 21:16:02 +0300

    add Logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+function Logo() {
+  return (
+    <div className="inline-flex items-center gap-2 pr-2">
+      <img src="/icon.svg" width={30} height={30} alt="" />
+      <p className="text-lg text-[#E63946]">FuegoMail</p>
+    </div>
+  );
+}
+export default Logo;
