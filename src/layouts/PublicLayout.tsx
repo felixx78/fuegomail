be8e0317c84c8750c9c4ca4f4143b0e90088e373
felixx78 <petrenkov605@gmail.com>
2024-07-30 23:18:02 +0300

    add PublicLayout

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,6 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import Logo from "../components/Logo";
+import ThemeToggler from "../components/ThemeToggler";
 
 function PublicLayout() {
-  return <Outlet />;
+  return (
+    <div className="bg-background dark:bg-dark-background text-primary-text dark:text-dark-primary-text min-h-svh">
+      <Header />
+      <Outlet />
+    </div>
+  );
 }
+
+const Header = () => {
+  return (
+    <div className="mb-4 flex items-center justify-between px-8 py-3">
+      <Link to="/">
+        <Logo />
+      </Link>
+
+      <ThemeToggler />
+    </div>
+  );
+};
+
 export default PublicLayout;
