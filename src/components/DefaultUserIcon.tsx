69c35bc0af7fa2d3fd4f6dc0bafe162772c378ec
felixx78 <petrenkov605@gmail.com>
2024-07-31 10:15:52 +0300

    add DefaultUserIcon

---
+++ src/components/DefaultUserIcon.tsx
@@ -0,0 +1,10 @@
+import { User } from "@phosphor-icons/react";
+
+function DefaultUserIcon() {
+  return (
+    <div className="w-[35px] rounded-full bg-gray-400 p-1 text-dark-primary-text dark:text-primary-text">
+      <User size={27} />
+    </div>
+  );
+}
+export default DefaultUserIcon;
