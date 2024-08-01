cdaddaea6651cf1f755dac9f46fdac4956c7d885
felixx78 <petrenkov605@gmail.com>
2024-08-01 21:43:02 +0300

    add /auth/sign-in

--- server/src/auth/index.ts
+++ server/src/auth/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import signUp from "./signUp";
+import signIn from "./signIn";
 
 const authRouter = express.Router();
 
 authRouter.post("/sign-up", ...signUp);
+authRouter.post("/sign-in", ...signIn);
 
 export default authRouter;
---
+++ server/src/auth/signIn.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { z } from "zod";
+import { validateSchema } from "../validateMiddleware";
+import bcrypt from "bcrypt";
+import client from "../../db";
+import generateToken from "../generateToken";
+import setTokenCookie from "../setTokenCookie";
+
+const schema = z.object({
+  username: z.string(),
+  password: z.string(),
+});
+
+async function signUp(req: Request, res: Response) {
+  const { username, password } = req.body as z.infer<typeof schema>;
+
+  const { rows: find } = await client.query(
+    "SELECT * from users WHERE username = $1",
+    [username]
+  );
+
+  if (!find.length) return res.status(400).json("Username doesn't exist");
+
+  if (!(await bcrypt.compare(password, find[0].password))) {
+    return res.status(403).json("Incorrect Password");
+  }
+
+  const refreshToken = await generateToken({ username }, "refresh");
+  const accessToken = await generateToken({ username }, "access");
+
+  setTokenCookie(res, refreshToken);
+  res.send(accessToken);
+}
+
+export default [validateSchema(schema), signUp];
