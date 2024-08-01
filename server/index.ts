796cf26c539a1a3c3ff44ea50528b9df0e95a5c4
felixx78 <petrenkov605@gmail.com>
2024-08-01 23:46:49 +0300

    move express.d.ts to index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,27 @@
 import express, { Application } from "express";
 import cors from "cors";
 import authRouter from "./src/auth";
+import authMiddleware from "./src/authMiddleware";
+import emailRouter from "./src/email";
 
 const app: Application = express();
 const port = 3000;
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        username: string;
+      };
+    }
+  }
+}
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRouter);
+app.use("/email", authMiddleware, emailRouter);
 
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
