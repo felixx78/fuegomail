914254bd0db1d2b22a5c9deecf7552d43267b26c
felixx78 <petrenkov605@gmail.com>
2024-08-01 17:21:16 +0300

    add response interceptor apiClient

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -2,5 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({ baseURL: "http://localhost:3000" });
 
+apiClient.interceptors.response.use((response) => response.data);
+
 const { get, post, put, delete: destroy } = apiClient;
 export { get, post, put, destroy };
