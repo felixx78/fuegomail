fa0066ae8321df68913aa55465690a3d45b57d50
felixx78 <petrenkov605@gmail.com>
2024-08-02 09:24:28 +0300

    update /api/index.ts

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -2,7 +2,41 @@ import axios from "axios";
 
 const apiClient = axios.create({ baseURL: "http://localhost:3000" });
 
-apiClient.interceptors.response.use((response) => response.data);
+const storedUser = localStorage.getItem("user");
+const user = storedUser && (JSON.parse(storedUser) as { token: string });
+
+apiClient.interceptors.request.use((config) => {
+  const token = user ? user.token : null;
+  if (token) {
+    //@ts-ignore
+    config.headers = { ...config.headers, Authorization: token };
+  }
+  return config;
+});
+apiClient.interceptors.response.use(
+  (response) => response.data,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const newToken = await get("/auth/refresh");
+        const { user: oldUser, token: _token } = user as any;
+        localStorage.setItem(
+          "user",
+          JSON.stringify({ user: oldUser, token: newToken }),
+        );
+        originalRequest.headers["Authorization"] = newToken;
+        return apiClient(originalRequest);
+      } catch (e) {
+        localStorage.removeItem("user");
+        window.location.href = "/sign-in";
+      }
+    }
+    return Promise.reject(error);
+  },
+);
 
 const { get, post, put, delete: destroy } = apiClient;
 export { get, post, put, destroy };
