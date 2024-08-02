e9999dca4f564b249fb6ff2af22f089715a90210
felixx78 <petrenkov605@gmail.com>
2024-08-02 20:51:42 +0300

    add link to Header logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import Logo from "./Logo";
 import ThemeToggler from "./ThemeToggler";
 
 function Header() {
   return (
     <div className="mb-2 flex items-center justify-between px-8 py-3">
-      <Logo />
+      <Link to="/">
+        <Logo />
+      </Link>
       <ThemeToggler />
     </div>
   );
